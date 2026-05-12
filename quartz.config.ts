8732c9f380cf55632dc1ba0d12f867062b902213
ValentinaKoroleva <valkorolyova@gmail.com>
2026-05-12 11:20:32 +0300

    Добавлены две первые рецензии из Книжного клуба Books and Reviews

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Reviews",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
